e09c6c47aa4c614d91340fdc05da4aee2e090193
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-04-04 14:51:36 -0300

    feat: adding select field for repositories

--- app.js
+++ app.js
@@ -1,64 +1,38 @@
-// Get the GitHub username input form
 const gitHubForm = document.getElementById('gitHubForm');
+let usernameInput = document.getElementById('username');
 
-// Listen for submissions on GitHub username input form
-gitHubForm.addEventListener('submit', (e) => {
+usernameInput.addEventListener('blur', () => {
+    let username = usernameInput.value;
 
-    // Prevent default form submission action
-    e.preventDefault();
-
-    // Get the GitHub username input field on the DOM
-    let usernameInput = document.getElementById('usernameInput');
-
-    // Get the value of the GitHub username input field
-    let gitHubUsername = usernameInput.value;
-
-    // Run GitHub API function, passing in the GitHub username
-    requestUserRepos(gitHubUsername)
-        .then(response => response.json()) // parse response into json
+    requestUserRepos(username)
+        .then(response => response.json())
         .then(data => {
-            // update html with data from github
-            for (let i in data) {
-                // Get the ul with id of userRepos
-
-                if (data.message === "Not Found") {
-                    let ul = document.getElementById('userRepos');
-
-                    // Create variable that will create li's to be added to ul
-                    let li = document.createElement('li');
-
-                    // Add Bootstrap list item class to each li
-                    li.classList.add('list-group-item')
-                    // Create the html markup for each li
-                    li.innerHTML = (`
-                <p><strong>No account exists with username:</strong> ${gitHubUsername}</p>`);
-                    // Append each li to the ul
-                    ul.appendChild(li);
-                } else {
-
-                    let ul = document.getElementById('userRepos');
-
-                    // Create variable that will create li's to be added to ul
-                    let li = document.createElement('li');
-
-                    // Add Bootstrap list item class to each li
-                    li.classList.add('list-group-item')
-
-                    // Create the html markup for each li
-                    li.innerHTML = (`
-                <p><strong>Repo:</strong> ${data[i].name}</p>
-                <p><strong>Description:</strong> ${data[i].description}</p>
-                <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
-            `);
-
-                    // Append each li to the ul
-                    ul.appendChild(li);
-                }
+            let select = document.getElementById('repository')
+            if (data.message === "Not Found") {
+                //TODO: show error message
+                select.disabled = true;
+            } else {
+                select.innerHTML = buildSelectOptions(data);
+                select.disabled = false;
             }
         })
 })
 
+
+gitHubForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
+function buildSelectOptions(repositories) {
+    let optionsHTML = "<option selected>Choose a repository</option>";
+
+    for (let repository of repositories) {
+        optionsHTML += `<option value="${repository.name}">${repository.name}</option>`;
+    }
+
+    return optionsHTML;
+}
+
 function requestUserRepos(username) {
-    // create a variable to hold the `Promise` returned from `fetch`
     return Promise.resolve(fetch(`https://api.github.com/users/${username}/repos`));
 }
